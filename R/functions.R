748a0435d70fa461ac50945dc05f1956b50ae1d3
Nils Kurzawa <nils.kurzawa@embl.de>
2019-12-17 18:21:16 +0100

    adding function for matrix summarization by e.g. median and accompanying tests

--- R/functions.R
+++ R/functions.R
@@ -27,6 +27,7 @@
 
 #' @importFrom dplyr select
 #' @importFrom dplyr filter
+#' @importFrom dplyr one_of
 .getMatList <- function(objList, commonRownames, rownameCol = NULL){
     if(class(objList[[1]])[1] == "ExpressionSet"){
         return(lapply(objList, function(x){
@@ -40,8 +41,10 @@
     }else if((class(objList[[1]])[1] %in% c("tbl_df")) & 
              !is.null(rownameCol)){
         return(lapply(objList, function(x){
-            x_fil <- filter(x, eval(parse(text = rownameCol)) %in% commonRownames) 
-            x_mat <- as.matrix(dplyr::select(x_fil, -one_of(rownameCol)))
+            x_fil <- filter(x, eval(parse(text = rownameCol)) %in% 
+                                commonRownames) 
+            x_mat <- as.matrix(dplyr::select(x_fil, 
+                                             -one_of(rownameCol)))
             rownames(x_mat) <- x_fil[[rownameCol]]
             return(x_mat)
         }))
@@ -51,6 +54,19 @@
     }
 }
 
-# .getSummarizedMat <- function(matList, FUN = median){
-#     
-# }
+.checkMatDims <- function(matList){
+    if(length(Reduce(intersect, lapply(matList, dim))) != 2){
+        stop("checkMatDims: unequal matrix dimensions!")
+    }
+}
+
+#' @importFrom stats median
+.getSummarizedMat <- function(matList, FUN = median){
+    .checkMatDims(matList)
+    arr <- array(unlist(matList), 
+                 c(dim(matList[[1]]), length(matList)))
+    sumMat <- apply(arr, seq_len(2), FUN)
+    rownames(sumMat) <- rownames(matList[[1]])
+    colnames(sumMat) <- colnames(matList[[1]])
+    return(sumMat)
+}
---
+++ tests/testthat/test-getSummarizedMat.R
@@ -0,0 +1,43 @@
+## Tests for .getCommonRownames
+## library(Rtpca); library(testthat)
+context(".getSummarizedMat")
+
+m1 <- matrix(1:12, ncol = 4)
+m2 <- matrix(2:13, ncol = 4)
+m3 <- matrix(c(2:10, 1:7), ncol = 4)
+
+rownames(m1) <- 1:3
+rownames(m2) <- 2:4
+rownames(m3) <- 2:5
+
+colnames(m1) <- paste0("X", 1:4)
+colnames(m2) <- paste0("X", 1:4)
+colnames(m3) <- paste0("X", 1:4)
+
+m1_new <- m1[-1,]
+m2_new <- m2[-3,]
+m3_new <- m3[-c(3,4),]
+
+mat_list <- list(
+    m1, m2, m3
+)
+
+mat_list_new <- list(
+    m1_new, m2_new, m3_new
+)
+
+ctrl_m <- matrix(
+    c(2, 3, 5, 6, 8, 9, 11, 12),
+    ncol = 4)
+rownames(ctrl_m) <- c("2", "3")
+colnames(ctrl_m) <- paste0("X", 1:4)
+
+expect_equal(
+    Rtpca::.getSummarizedMat(mat_list_new),
+    ctrl_m
+)
+
+expect_error(
+    Rtpca::.getSummarizedMat(mat_list),
+    "checkMatDims: unequal matrix dimensions!"
+)
