4eed4424498fc60d19eb03e417cacd10dcaecc94
Nils Kurzawa <nils.kurzawa@embl.de>
2020-03-21 13:50:11 +0100

    adapted test to bug fix in F statistic computation

--- tests/testthat/test-combineCondDistMatsFstat.R
+++ tests/testthat/test-combineCondDistMatsFstat.R
@@ -23,11 +23,11 @@ ctrl_df <- tibble(
     pair = "2:3",
     valueC1 = 2,
     valueC2 = 0.9,
-    rssC1 = sqrt(2),
-    rssC2 = sqrt(0.9),
-    rssC1_rssC2 = sqrt(2) - sqrt(0.9),
-    min_rssC1_rssC2 = sqrt(0.9),
-    f_stat = (sqrt(2) - sqrt(0.9))/sqrt(0.9)
+    rssC1 = 2^2,
+    rssC2 = 0.9^2,
+    rssC1_rssC2 = 2^2 - 0.9^2,
+    min_rssC1_rssC2 = 0.9^2,
+    f_stat = (2^2 - 0.9^2)/0.9^2
 )
 
 expect_equal(
